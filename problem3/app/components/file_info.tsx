14312c131670ed0dec109badf05494435c7623c0
Trong Tran <gacontuyenchien1@gmail.com>
2023-11-26 14:26:55 +0700

    solution 3: complete

--- problem3/app/components/file_info.tsx
+++ problem3/app/components/file_info.tsx
@@ -3,14 +3,15 @@ import React, { useEffect, useState, useMemo } from "react";
 import { CloudArrowUpIcon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
 import { v4 as uuidv4 } from "uuid";
-import axios from "axios";
+import { toast, TypeOptions as ToastTypeOptions } from "react-toastify";
 
-// import { v4 as uuidv4 } from 'uuid';
+const NUMB_OF_TOP_WORDS = 3;
 
 const FileInfo = () => {
+    const TIME_SHOWING_TOAST = 2000; // in milliseconds,
     const [file, setFile] = useState<File | null>(null);
     const [shouldHighlight, setShouldHighlight] = useState(false);
-    const [allowChecking, setAllowChecking] = useState(false);
+    const [preventChecking, setPreventChecking] = useState(true);
     const [fileDetail, setFileDetail] = useState<FileCountedDetail | null>();
     const [curFileChecking, setCurFileChecking] =
         useState<FileToCheck | null>();
@@ -28,8 +29,23 @@ const FileInfo = () => {
         e.stopPropagation();
     };
 
+    const notify = (_mess: String, _type: ToastTypeOptions) => {
+        toast(_mess, {
+            hideProgressBar: true,
+            autoClose: TIME_SHOWING_TOAST,
+            type: _type,
+        });
+    };
+
     const onDropAFile = async (_file: File) => {
         if (_file === null) return;
+        // Prevent not txt files
+        if (!_file.name.endsWith(".txt")) {
+            notify("File must in a .txt extension", "error");
+            setPreventChecking(true);
+            return;
+        }
+        setPreventChecking(false);
         const fileContent: String | null = await readFileAsync(_file);
 
         const _droppedFile = {
@@ -38,20 +54,9 @@ const FileInfo = () => {
             content: fileContent,
         } as FileToCheck;
         setDroppedFile(_droppedFile);
-        console.error("mot hai ba");
-        // Prevent not txt files
-        // Read the file contents
-        // Check
-        // Save the file contents
     };
 
     const handleChecking = async () => {
-        // Get string data from dropped file
-        const UPLOAD_URL = "/api/upload";
-        const data = new FormData();
-        data.append(file!.name, file!);
-
-        await axios.post(UPLOAD_URL, data);
         // Process checking
         // Set current file checking, to receive only the last checking result
         setCurFileChecking(droppedFile);
@@ -59,13 +64,57 @@ const FileInfo = () => {
         if (window.Worker) {
             textChecker.postMessage(droppedFile);
         }
+        notify("Starting to checking file...", "success");
     };
+
+    function fileDetailComponent() {
+        const topWords: Array<WordDetail> =
+            fileDetail?.fileCountedDetail?.topWords || [];
+        if (topWords.length === 0) {
+            for (let i = 0; i < NUMB_OF_TOP_WORDS; i++) {
+                topWords.push({
+                    word: "",
+                    count: 0,
+                } as WordDetail);
+            }
+        }
+        return (
+            <div className="flex flex-col items-start justify-between p-24 w-full">
+                <div className="flex gap-2 mt-2 w-64 my-3">
+                    <span className="w-full">Different words:</span>
+                    <span className="w-32 text-end font-bold">
+                        {fileDetail?.fileCountedDetail.diffWords}
+                    </span>
+                </div>
+                <div>Top words:</div>
+                {topWords.map((wordDetail, id) => {
+                    return (
+                        <div className="flex gap-2 mt-2 w-64" key={id}>
+                            <span className="w-full ml-10 italic">
+                                {wordDetail.word}
+                            </span>
+                            <span className="w-32 text-end font-bold">
+                                {wordDetail.count === 0 ? "" : wordDetail.count}
+                            </span>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
 
     useEffect(() => {
         if (window.Worker) {
             textChecker.onmessage = (e: MessageEvent<FileCountedDetail>) => {
-                setFileDetail(e.data);
-                console.log("Received from worker: " + e.data);
+                const _fileDetail = e.data as FileCountedDetail;
+                // If the result is matching the current dropped file
+                if (_fileDetail.id === droppedFile?.id) {
+                    setFileDetail(_fileDetail);
+                }
+                setFileDetail(_fileDetail);
+                console.log("fileDetail: ", _fileDetail);
+                notify("Calculate done", "info");
+                setPreventChecking(true);
             };
         }
     }, []);
@@ -94,69 +143,86 @@ const FileInfo = () => {
     return (
         <div
             className={classNames({
-                "w-full h-96": true,
-                "p-4 grid place-content-center cursor-pointer": true,
+                "w-full h-full": true,
+                "justify-center": true,
+                "p-4 grid cursor-pointer": true,
                 "text-violet-500 rounded-lg": true,
-                "border-4 border-dashed ": true,
+                "border-4": true,
                 "transition-colors": true,
-                "border-violet-500 bg-violet-100": shouldHighlight,
-                "border-violet-100 bg-violet-50": !shouldHighlight,
             })}
-            onDragOver={(e) => {
-                preventDefaultHandler(e);
-                setShouldHighlight(true);
-            }}
-            onDragEnter={(e) => {
-                preventDefaultHandler(e);
-                setShouldHighlight(true);
-            }}
-            onDragLeave={(e) => {
-                preventDefaultHandler(e);
-                setShouldHighlight(false);
-            }}
-            onDrop={(e) => {
-                // UI logic, get only one file from the drop
-                preventDefaultHandler(e);
-                const file = e.dataTransfer.files[0];
-                setFile(file);
-                setShouldHighlight(false);
-                // Do logic businesses
-                onDropAFile(file);
-            }}
         >
-            <div className="flex flex-col items-center">
-                {!file ? (
-                    <>
-                        <CloudArrowUpIcon className="w-10 h-10" />
-                        <span>
-                            <span>Choose a File</span> or drag it here
-                        </span>
-                    </>
-                ) : (
-                    <>
-                        <span key={0}>{file.name}</span>
-                        <div className="flex gap-2 mt-2">
-                            <button
-                                className="bg-violet-500 text-violet-50 px-2 py-1 rounded-md disabled:via-violet-50 hover:bg-violet-400"
-                                disabled={allowChecking}
-                                onClick={() => {
-                                    handleChecking();
-                                }}
-                            >
-                                Check
-                            </button>
-                            <button
-                                className="border border-violet-500 px-2 py-1 rounded-md"
-                                onClick={() => {
-                                    setFile(null);
-                                }}
-                            >
-                                Clear
-                            </button>
-                        </div>
-                    </>
-                )}
+            <div className="w-full text-6xl text-center my-20">
+                Word Counter
             </div>
+            <div
+                className={classNames({
+                    "flex-1 h-80": true,
+                    "p-4 grid place-content-center cursor-pointer": true,
+                    "text-violet-500 rounded-lg": true,
+                    "border-4 border-dashed ": true,
+                    "transition-colors": true,
+                    "border-violet-500 bg-violet-100": shouldHighlight,
+                    "border-violet-100 bg-violet-50": !shouldHighlight,
+                })}
+                onDragOver={(e) => {
+                    preventDefaultHandler(e);
+                    setShouldHighlight(true);
+                }}
+                onDragEnter={(e) => {
+                    preventDefaultHandler(e);
+                    setShouldHighlight(true);
+                }}
+                onDragLeave={(e) => {
+                    preventDefaultHandler(e);
+                    setShouldHighlight(false);
+                }}
+                onDrop={(e) => {
+                    // UI logic, get only one file from the drop
+                    preventDefaultHandler(e);
+                    const file = e.dataTransfer.files[0];
+                    setFile(file);
+                    setShouldHighlight(false);
+                    // Do logic businesses
+                    onDropAFile(file);
+                }}
+            >
+                <div className="flex flex-col items-center">
+                    {!file ? (
+                        <>
+                            <CloudArrowUpIcon className="w-10 h-10" />
+                            <span>
+                                <span>Choose a File</span> or drag it here
+                            </span>
+                        </>
+                    ) : (
+                        <>
+                            <span key={0}>{file.name}</span>
+                            <div className="flex gap-2 mt-2">
+                                <button
+                                    className="enabled:bg-violet-500 text-violet-50 px-2 py-1 rounded-md bg-violet-300 enabled:hover:bg-violet-400"
+                                    disabled={preventChecking}
+                                    onClick={() => {
+                                        handleChecking();
+                                    }}
+                                >
+                                    Check
+                                </button>
+                                <button
+                                    className="border border-violet-500 px-2 py-1 rounded-md"
+                                    onClick={() => {
+                                        setFile(null);
+                                        setDroppedFile(null);
+                                        setFileDetail(null);
+                                    }}
+                                >
+                                    Clear
+                                </button>
+                            </div>
+                        </>
+                    )}
+                </div>
+            </div>
+            {fileDetailComponent()}
         </div>
     );
 };
--- problem3/app/workers/file_checker.ts
+++ problem3/app/workers/file_checker.ts
@@ -1,23 +1,31 @@
 type FileToCheck = {
-    id: String;
-    fileName: String;
-    content: String | null;
+    id: string;
+    fileName: string;
+    content: string | null;
 };
 
 type FileCountedDetail = {
-    id: String;
-    fileName: String;
-    totalWord: number;
-    mostRepeatedWords: Array<WordDetail>;
-    errorMessage: String | null;
+    id: string; // Avoid getting result from other worker
+    fileName: string;
+    fileCountedDetail: ListWordCountedDetail;
 };
 
 type WordDetail = {
-    word: String;
+    word: string;
     count: number;
 };
 
-// Input: a String containing alphabets, separated: dot (.), comma (,), space ( ).
+type ListWordCountedDetail = {
+    diffWords: number;
+    topWords: Array<WordDetail>; // From larger time to smaller
+    errorMessage: string | null;
+};
+
+const WORD_SEPARATORS = /[.|,|\s]+/;
+const CORRECT_TEXT_PATTERN = /^[a-zA-Z.,\s]+$/;
+const NUMB_OF_TOP_WORDS = 3;
+
+// Input: a string containing alphabets, separated: dot (.), comma (,), space ( ).
 // Output: FileCountedDetails object
 self.onmessage = (event) => {
     const data: FileToCheck = event.data;
@@ -25,19 +33,81 @@ self.onmessage = (event) => {
     console.log(
         "Worker: Received the text of file name, ",
         data.fileName,
-        ", id: ",
+        "\n, id: ",
         data.id,
-        ", content: ",
+        "\n, content: ",
         data.content,
     );
-    const result: FileCountedDetail = {
-        id: "hihi",
-        fileName: data.fileName,
-        totalWord: 0,
-        mostRepeatedWords: [],
-        errorMessage: null,
-    };
+
+    const result = wordCounter(data.content);
+    result.id = data.id;
+    result.fileName = data.fileName;
 
     console.log("Problem3: Worker done!");
     postMessage(result);
+};
+
+const wordCounter = (_text: string | null): FileCountedDetail => {
+    const result: FileCountedDetail = {
+        id: "",
+        fileName: "",
+        fileCountedDetail: {
+            diffWords: 0,
+            topWords: [],
+            errorMessage: null,
+        },
+    };
+    // Check input null
+    if (_text === null) {
+        result.fileCountedDetail.errorMessage =
+            "The file contains no more than 3 different words";
+        return result;
+    }
+    // Check correct input
+    if (_text !== null && !CORRECT_TEXT_PATTERN.test(_text)) {
+        result.fileCountedDetail.errorMessage =
+            "The file must contains only alphabetical characters and separator as dot, comma and whitespace characters";
+        return result;
+    }
+    // Count the number of distinct words
+    const words = _text.split(WORD_SEPARATORS);
+    // Count the most repeated words
+    result.fileCountedDetail = countWords(words);
+    return result;
+};
+
+const countWords = (wordList: Array<string>): ListWordCountedDetail => {
+    const rs: ListWordCountedDetail = {
+        diffWords: 0,
+        topWords: [],
+        errorMessage: null,
+    };
+    // Calculate a map<word, number> from the wordList
+    const wordsMap: Map<string, number> = new Map();
+    for (const word of wordList) {
+        wordsMap.set(word, (wordsMap.get(word) || 0) + 1);
+    }
+    rs.diffWords = wordsMap.size;
+    if (rs.diffWords < 3) {
+        rs.errorMessage = "The file contains less than 3 different words";
+        return rs;
+    }
+    // Get top-level words
+    const topWords: Array<string> = [];
+    for (let i = 0; i < NUMB_OF_TOP_WORDS; i++) {
+        const topWordDt: WordDetail = {
+            word: "",
+            count: 0,
+        };
+        for (const word of wordsMap.keys()) {
+            const _count = wordsMap.get(word) || 0;
+            if (_count > topWordDt.count && !topWords.includes(word)) {
+                topWordDt.word = word;
+                topWordDt.count = _count;
+                topWords.push(topWordDt.word);
+            }
+        }
+        rs.topWords.push(topWordDt);
+    }
+    return rs;
 };
