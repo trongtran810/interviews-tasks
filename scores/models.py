01ad3ca57286897ee49a566aa5e2935726ed661e
Trong Tran <gacontuyenchien1@gmail.com>
2023-12-04 01:22:19 +0700

    init django

---
+++ scores/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+class UserScore(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
---
+++ sql_scripts.sql
@@ -0,0 +1,25 @@
+-- Create User table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- Create UserScore table
+CREATE TABLE user_scores (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) UNIQUE,
+    score INTEGER DEFAULT 0
+);
+
+-- Add sample data for users
+INSERT INTO users (username, email) VALUES
+    ('john_doe', 'john@example.com'),
+    ('jane_smith', 'jane@example.com'),
+    ('bob_johnson', 'bob@example.com');
+
+-- Add sample data for user scores
+INSERT INTO user_scores (user_id, score) VALUES
+    (1, 100),
+    (2, 150),
+    (3, 120);
